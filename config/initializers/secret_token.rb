09b2bab2c4f8f7b83745092d4f2b681551ca2ad1
Tyler Ewing <tewing10@gmail.com>
2014-03-04 15:29:52 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HereDoggie::Application.config.secret_key_base = '65d10eac1bddd0de1b312b9a93345cac80debfe612a4c1f433badaf6643341ef80978ac32118da5f0e9ce1e0fb87c3d64bfb7478a9a455870833228bac3afd06'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HereDoggie::Application.config.session_store :cookie_store, key: '_HereDoggie_session'
