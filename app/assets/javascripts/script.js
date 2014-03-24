7189c7a4ebcc9a5d2ec52a3b75e81ad2953ba7b3
David Ecker <dcecker@gmail.com>
2014-03-23 21:24:37 -0400

    Fixed preview image sizing

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -4,21 +4,21 @@ $(document).ready(function(){
 	$('.datepicker').datepicker();
 
 	function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        
-        reader.onload = function (e) {
-            $('#img-preview').attr('src', e.target.result);
-        }
-        
-        reader.readAsDataURL(input.files[0]);
-    }
-}
+	    if (input.files && input.files[0]) {
+	        var reader = new FileReader();
+	        
+	        reader.onload = function (e) {
+	            $('#img-preview').attr('src', e.target.result);
+	        }
+	        
+	        reader.readAsDataURL(input.files[0]);
+	    }
+	}
 
-$("#imgInp").change(function(){
-    readURL(this);
-    $("#img-preview").css("opacity", 1);
-});
+	$("#imgInp").change(function(){
+	    readURL(this);
+	    $("#img-preview").css("opacity", 1);
+	});
 });
 
 
