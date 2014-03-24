28e409949346ec1606ab05824a94794f7be0fbb5
David Ecker <dcecker@gmail.com>
2014-03-23 20:53:49 -0400

    Added bootstrap 3 goodies

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('input[type=file]').bootstrapFileInput();
+	$('.file-inputs').bootstrapFileInput();	
+	$('.datepicker').datepicker();
+
+	function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        
+        reader.onload = function (e) {
+            $('#img-preview').attr('src', e.target.result);
+        }
+        
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#imgInp").change(function(){
+    readURL(this);
+    $("#img-preview").css("opacity", 1);
+});
+});
+
+
