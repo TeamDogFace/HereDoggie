b2b8d348ec37f6ff2136abd422ac489437eb306b
Tyler Ewing <tewing10@gmail.com>
2014-03-25 08:41:39 -0400

    Updated Vagrant bootstrap to install ImageMagick

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 echo "Setting up Ruby on Rails dev environment"
-apt-get update >/dev/null 2>&1
+apt-get update > /dev/null 2>&1
 echo "Installing curl"
-apt-get install -y curl
+apt-get install -y curl > /dev/null 2>&1
 echo "Installing a JavaScript environment"
-apt-get install -y nodejs
+apt-get install -y nodejs > /dev/null 2>&1
 echo "Downloading and Installing RVM"
-curl -L https://get.rvm.io | bash -s stable --ruby
+curl -L https://get.rvm.io | bash -s stable --ruby > /dev/null
 source /usr/local/rvm/scripts/rvm
 echo "We want a Ruby version >2.0"
 echo `ruby -v`
-
 echo "Installing Rails"
-gem install rails --no-ri --no-rdoc
+gem install rails --no-ri --no-rdoc > /dev/null
 echo "We want a Rails version >4.0"
 echo `rails -v`
-
+echo "Installing ImageMagick"
+apt-get install -y imagemagick > /dev/null 2>&1
