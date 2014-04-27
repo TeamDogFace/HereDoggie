10b765344a39f42455729ba74247d4df8e5cfdb8
David Ecker <dcecker@gmail.com>
2014-04-27 16:46:37 -0400

    added ruby support for calling php crawler

--- app/workers/facial_recognition_worker.rb
+++ app/workers/facial_recognition_worker.rb
@@ -3,6 +3,9 @@ class FacialRecognitionWorker
   sidekiq_options retry: false
 
   def perform(search_id)
+  	# Run php crawler script
+  	logger.info `cd vendor/FacebookCrawler; php crawler.php; cd ..`
+
     # Run python script for facial recognition
     # with the search id as a command-line argument
     logger.info `cd vendor/DogFaceRecognizer; python main.py #{search_id}`
