ff40d31cad8b5d26e717958c4fcfa57ee4746371
Tyler Ewing <tewing10@gmail.com>
2014-07-09 20:34:09 -0400

    Update routes and login user after registration

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,19 @@ require 'sidekiq/web'
 HereDoggie::Application.routes.draw do
 
   # Static pages and Landing Page
-  get "static/about"
-  get "static/faq"
-  get "static/landing"
+  get "about", to: "static/about"
+  get "faq", to: "static/faq"
+  get "landing", to: "static/landing"
 
   # Dog Searches
   resources :searches
 
   # Users and Sessions
   resources :users
+  get "register", to: "users#new"
   resources :sessions, only: [:new, :create, :destroy]
-  get "login", to: "sessions#create"
-  get "exit", to: "sessions#destroy", as: 'logout'
+  get "login", to: "sessions#new"
+  get "exit", to: "sessions#destroy", as: "logout"
 
   root "static#landing"
 
